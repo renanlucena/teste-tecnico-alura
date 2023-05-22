a47cb4645b1549684c3c5e87265015afea53dbc5
renanlucena <renan.segura.de.lucena@gmail.com>
2023-05-22 15:03:17 -0300

    implementando e estilizando o rodapé da página

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,6 +1,7 @@
 import Banner from "../../componentes/Banner";
 import Cabecalho from "../../componentes/Cabecalho";
 import Menu from "../../componentes/Menu";
+import Rodape from "../../componentes/Rodape";
 import styles from "./PaginaInicial.module.scss";
 
 export default function PaginaInicial(){
@@ -11,6 +12,7 @@ export default function PaginaInicial(){
             <section className={styles.principal}>
                 <Menu/>
                 <Banner/>
+                <Rodape />
             </section>
         </main>
         
