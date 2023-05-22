3ab6c385c7ecbc99f6f3a142715133dbce39a79f
renanlucena <renan.segura.de.lucena@gmail.com>
2023-05-22 10:50:14 -0300

    criando a eestrutura html do cabeçalho

--- src/componentes/Cabecalho/index.jsx
+++ src/componentes/Cabecalho/index.jsx
@@ -1,5 +1,14 @@
+import logo from './logo.png';
+import search from './search.png';
+
 export default function Cabecalho (){
     return(
-        <h1>Cabecalho</h1>
+        <header>
+            <img src={logo} alt="Logo do Alura Space" />
+            <div>
+                <input type="text" placeholder="O que você procura?" />
+                <img src= {search} alt="Icone de lupa" />
+            </div>
+        </header>
     )
 }
