7f0b9fe53983fefba20e19a6745758de3b0d1ed1
renanlucena <renan.segura.de.lucena@gmail.com>
2023-05-23 10:18:20 -0300

    implementando o componente Populares

---
+++ src/componentes/Populares/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from './Populares.module.scss';
+import fotosPopulares from './fotos-populares.json';
+
+export default function Populares() {
+    return (
+        <aside className={styles.populares}>
+            <h2>Populares</h2>
+            <ul className={styles.populares__imagens}>
+                {fotosPopulares.map(foto => {
+                    return (
+                        <li key={foto.id}>
+                            <img src={foto.path} alt={foto.alt} />
+                        </li>
+                    );
+                })}
+
+            </ul>
+            <button>Ver mais fotos</button>
+        </aside>
+    );
+}
--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -2,25 +2,28 @@ import Banner from "../../componentes/Banner";
 import Cabecalho from "../../componentes/Cabecalho";
 import Galeria from "../../componentes/Galeria";
 import Menu from "../../componentes/Menu";
+import Populares from "../../componentes/Populares";
 import Rodape from "../../componentes/Rodape";
 import styles from "./PaginaInicial.module.scss";
 
-export default function PaginaInicial(){
-    return(
+export default function PaginaInicial() {
+    return (
         <>
-        <Cabecalho/>
-        <main>
-            <section className={styles.principal}>
-                <Menu/>
-                <Banner/>              
-            </section>
-        </main>
-        <div className={styles.galeria}>
-            <Galeria />
-        </div>
-        <Rodape />
-    
-        
+            <Cabecalho />
+            <main>
+                <section className={styles.principal}>
+                    <Menu />
+                    <Banner />
+                </section>
+                <div className={styles.galeria}>
+                    <Galeria />
+                    <Populares />
+                </div>
+            </main>
+
+            <Rodape />
+
+
         </>
     )
 }
