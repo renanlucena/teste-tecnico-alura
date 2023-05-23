d2414f279d158c9767380a23d8ebdc6ea3b20c0b
renanlucena <renan.segura.de.lucena@gmail.com>
2023-05-23 10:42:23 -0300

    utilizando jsconfig para importar componentes

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,9 +1,10 @@
-import Banner from "../../componentes/Banner";
-import Cabecalho from "../../componentes/Cabecalho";
-import Galeria from "../../componentes/Galeria";
-import Menu from "../../componentes/Menu";
-import Populares from "../../componentes/Populares";
-import Rodape from "../../componentes/Rodape";
+import Banner from "componentes/Banner";
+import Cabecalho from "componentes/Cabecalho";
+import Galeria from "componentes/Galeria";
+import Menu from "componentes/Menu";
+import Populares from "componentes/Populares";
+import Rodape from "componentes/Rodape";
+
 import styles from "./PaginaInicial.module.scss";
 
 export default function PaginaInicial() {
